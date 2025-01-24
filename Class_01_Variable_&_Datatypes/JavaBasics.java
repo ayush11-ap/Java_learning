722d6c0e32f4e69e0011a4866b92a7d6f58ef3ea
Ayush Pawar <ayushap1105@gmail.com>
2025-01-24 11:40:10 +0530

    Day 1 of learning Data Types & Variable

--- Class_01_Variable_&_Datatypes/JavaBasics.java
+++ Class_01_Variable_&_Datatypes/JavaBasics.java
@@ -1,6 +1,73 @@
+import java.util.*;
+
+// public class JavaBasics {
+//     public static void main(String args[]) {
+//         System.out.print("Hello, Ayush Pawar\n");
+//         System.out.println("Hello, Ayush Pawar");
+//         System.out.println("Hello, Ayush Pawar");
+//     }
+// }
+
+// public class JavaBasics {
+//     public static void main(String args[]){
+//         System.out.println("* * * *");
+//         System.out.print("* * *\n");
+//         System.out.println("* *");
+//         System.out.print("*\n");
+//     }
+// }
+
+import java.util.Scanner;
+
 public class JavaBasics {
     public static void main(String args[]) {
-        System.out.print("Hello, Ayush Pawar");
+        // int a = 11;
+        // int b = 12;
+        // String name = "Ayush Pawar";
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(name);
+
+        // a = 2005;
+        // System.out.println(a);
+
+        // Data Types
+        // byte b = 8;
+        // System.out.println(b);
+        // char ch = 'a';
+        // System.out.println(ch);
+        // boolean flag = true;
+        // System.out.println(flag);
+        // double price = 10.5;
+        // int number = 255;
+        // short n = 240;
+
+        // int a = 6;
+        // int b = 5;
+        // int sum = a + b;
+        // System.out.println(sum);
+        /* */
+
+        // inputs
+        // Scanner sc = new Scanner(System.in);
+        // long inp = sc.nextLong();
+        // System.out.println(inp);
+
+        // sum of a and b taking input
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a + b;
+        // int product = a * b;
+        // System.out.println(product);
+
+        Scanner sc = new Scanner(System.in);
+        float pi = 3.14f;
+        float radius = sc.nextFloat();
+        float area = pi * radius * radius;
+        System.out.println(area);
+
+        // Completed till 6.12 Area of circle
     }
 }
 
